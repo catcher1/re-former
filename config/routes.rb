94821fa8b278c6e36e8440778cf5379510aad22c
catcher1 <danlau168@gmail.com>
2015-06-26 13:25:08 -0700

    Finish create and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
-
   get 'users/create'
+  get 'users/edit'
 
-  resources :users, only: [ :new, :create ]
+  resources :users, only: [ :new, :create, :edit, :update ]
 end
